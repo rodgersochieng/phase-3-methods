b9150d3e57a3d300dbb8b37e0d9db0549c789000
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-18 23:29:02 +0300

    complted lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
-# Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end 
+greet_programmer
+
+
+def greet (name = "john")
+    puts "Hello, #{name}!"
+
+end 
+ greet
+
+#  def greet_with_default (name = "Programmer")
+#     puts "Hello, #{name}!"
+#  end
+#  greet_with_default 
+#  greet_with_default "Naureen"
+def greet_with_default(name = "programmer") 
+    puts "Hello, #{name}!"
+  end
+
+  greet_with_default
+
+  greet_with_default "Naureen"
+
+ def add (num1,num2)
+    return num1 + num2
+ end
+ add(2,4)
+      
+    def halve(number)
+        return nil unless number.class == Integer
+    
+        number/2
+      end
+
+
